60d8f0f629ef67be9010e51ca44a985e94a47a4e
Bill Lenoir <bill@bill-lenoir.com>
2024-09-29 15:49:51 -0400

    Initial Push

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,24 @@
-import { javascript } from "projen";
 import { monorepo } from "@aws/pdk";
+import { javascript, typescript } from "projen"; // <- change this line
+
 const project = new monorepo.MonorepoTsProject({
   devDeps: ["@aws/pdk"],
-  eslint: true,
   github: true,
-  name: "game-collection-data",
+  eslint: true,
+  name: "projen7-tms",
   packageManager: javascript.NodePackageManager.NPM,
   prettier: true,
   projenrcTs: true,
 });
+
+// Add the following:
+new typescript.TypeScriptAppProject({
+  parent: project,
+  name: "typescript-data-app",
+  defaultReleaseBranch: "main",
+  outdir: "data-app",
+  packageManager: project.package.packageManager,
+});
+
+// This MUST be last and only called once
 project.synth();
